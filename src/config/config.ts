273c6f7aee0da3122fd71941b2ecf670c24a4a40
Your Name <you@example.com>
2025-05-16 10:32:42 +0700

    fix: api

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 const config = {
-  BASE_URL: 'http://192.168.100.99:5555',
-  // BASE_URL: 'https://apiedulite.simsmk.sch.id',
-  BASE_MEDIA_URL: 'http://192.168.100.99/edulite/include/media'
-  // BASE_MEDIA_URL: 'https://simsmk.sch.id/edulite/include/media'
+  // BASE_URL: 'http://192.168.100.99:5555',
+  // BASE_MEDIA_URL: 'http://192.168.100.99/edulite/include/media'
+  BASE_URL: 'https://apiedulite.simsmk.sch.id',
+  BASE_MEDIA_URL: 'https://simsmk.sch.id/edulite/include/media'
 } as const
 
 export default config
