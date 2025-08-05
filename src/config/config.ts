b13eacf03032fb1d1283b22ea4dc496080eaa157
Your Name <you@example.com>
2025-08-05 07:01:12 +0700

    update: reorder and uncomment BASE_URL and BASE_MEDIA_URL in configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 const config = {
-  BASE_URL: ' http://localhost:10001/',
-  BASE_MEDIA_URL: 'http://localhost/santrinet/inc/media/'
-  // BASE_URL: 'https://api-santrinet.simsmk.sch.id',
-  // BASE_MEDIA_URL: 'https://simsmk.sch.id/santrinet-uploads'
+  // BASE_URL: ' http://localhost:10001/',
+  // BASE_MEDIA_URL: 'http://localhost/santrinet/inc/media/'
+  BASE_URL: 'https://api-santrinet.simsmk.sch.id',
+  BASE_MEDIA_URL: 'https://simsmk.sch.id/santrinet-uploads'
 } as const
 
 export default config
